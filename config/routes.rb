47291b72ed1ee3f421482ce630a8b908b8cbcf21
Arunraj <arunraj@mobme.in>
2011-09-16 14:34:25 +0530

    Finished first cut of user model

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ SampleApp::Application.routes.draw do
   match '/help', :to=>'pages#help'
   #get "user/new"
   match '/signup', :to=>'users#new'
+  resources :users
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
